4d79617571599e257b134d2957ea287d448cc1b4
John <54johnmbugua@gmail.com>
2025-03-16 10:45:57 +0300

    second commit

--- api/index.js
+++ api/index.js
@@ -15,7 +15,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(
     cors({
-        origin: 'http://localhost:5173',
+        origin: 'https://johnny-e0va.onrender.com/',
         credentials: true,
     }),
 );
